b180c9309ff3852a076d7a33ad01d01a59aa69e9
harikrishnan-dh <harivijay0411@gmail.com>
2025-11-26 09:58:14 +0530

    add the code

--- main.js
+++ main.js
@@ -4,22 +4,21 @@ document.querySelector("button").addEventListener("click", function () {
     let emiAmount = parseFloat(document.getElementById("emiAmount").value);
     let loanTenure = parseFloat(document.getElementById("loanTenure").value);
 
-    // Convert yearly interest rate to monthly
-    let monthlyRate = interestRate / 12 / 100;
-
-    // Convert years to months
+    
+    let monthlyRate = interestRate / 12 / 1
+    
     let months = loanTenure * 12;
 
-    // EMI formula
+    
     let emi =
         (loanAmount * monthlyRate * Math.pow(1 + monthlyRate, months)) /
         (Math.pow(1 + monthlyRate, months) - 1);
 
-    // Total payment & interest
+    
     let totalPayment = emi * months;
     let totalInterest = totalPayment - loanAmount;
 
-    // Update HTML output
+    
     document.getElementById("emi").innerText = emi.toFixed(2);
     document.getElementById("interest").innerText = totalInterest.toFixed(2);
     document.getElementById("payment").innerText = totalPayment.toFixed(2);
